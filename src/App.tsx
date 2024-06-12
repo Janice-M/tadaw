325fcfee5242d1be6342e87ac10899c4481b4804
Janice-M <janiceink001@gmail.com>
2024-06-12 12:39:37 +0300

    ts init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 
 const App: React.FC =() => {
+
+
   return(
     <div className="App">
+      <div className="header">
+        <div className="inputContainer">
+          <input type="text" name="task" placeholder="Add a Tadaw" />
+
+        </div>
+      </div>
 
     </div>
   );
